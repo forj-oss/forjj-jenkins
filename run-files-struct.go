d54e67b9de027600f0669e87ed4bf537390304e1
Christophe Larsonneur <clarsonneur@gmail.com>
2018-06-15 14:55:49 +0200

    Be able to create files when before running maintain command

---
+++ run-files-struct.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"github.com/forj-oss/goforjj"
+	"log"
+	"os"
+)
+
+// RunFilesStruct List of files
+type RunFilesStruct map[string]RunFileStruct
+
+// RunFileStruct detailled a File content and actions
+type RunFileStruct struct {
+	RemoveWhenDone bool `yaml:"remove-when-done"`
+	EnvStruct
+}
+
+// createFiles loop on files to create them if needed.
+func (fs RunFilesStruct)createFiles(model *JenkinsPluginModel, ret *goforjj.PluginData) error {
+	for key, env_to_set := range fs {
+		if env_to_set.If != "" {
+			// check if If evaluation return something or not. if not, the environment key won't be created.
+			if v, err := Evaluate(env_to_set.If, model); err != nil {
+				ret.Errorf("Error in evaluating '%s'. %s", key, err)
+			} else {
+				if v == "" {
+					continue
+				}
+			}
+		}
+		if v, err := Evaluate(env_to_set.Value, model); err != nil {
+			ret.Errorf("Error in evaluating '%s'. %s", key, err)
+		} else {
+			if err := env_to_set.createFile(key, v) ; err != nil {
+				return fmt.Errorf(ret.Errorf("%s", err))
+			}
+			fd, err := os.Create(key)
+			if err != nil {
+				return fmt.Errorf("Unable to create %s. %s", key, err)
+			}
+			
+			fd.WriteString(v)
+		}
+	}
+	return nil
+}
+
+// createFiles loop on files to create them if needed.
+func (fs RunFilesStruct)deleteFiles() error {
+	for name, fileState := range fs {
+		if fileState.RemoveWhenDone  {
+			// remove the file created.
+			os.Remove(name)
+		}
+	}
+	return nil
+}
+
+
+func (f RunFileStruct) createFile(name, value string) error {
+	fd, err := os.Create(name)
+	if err != nil {
+		return fmt.Errorf("Unable to create %s. %s", name, err)
+	}
+	defer fd.Close()
+	
+	fd.WriteString(value)
+	log.Printf("%s created", name)
+	return nil
+}
